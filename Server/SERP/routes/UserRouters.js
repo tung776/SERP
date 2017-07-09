d8fcd47dd42fa932b885360f8a31eb4d1731ee09
tung776 <thanhtung776@gmail.com>
2017-07-09 22:21:09 +0700

    xac minh trung username hoac email tren server

--- Server/SERP/routes/UserRouters.js
+++ Server/SERP/routes/UserRouters.js
@@ -1,35 +1,66 @@
 import express, {Router} from 'express';
 const UserRouters = Router();
-import validateInput from '../validators/SignupValidator';
+import commonValidation from '../validators/SignupValidator';
 import bcrypt from 'bcrypt';
+// import Promise from 'bluebird';
 import UserModel from '../models/UserModel';
+import  isEmpty from 'lodash/isEmpty';
+
+function validateInput(data, otherValidation) {
+    let { errors } = otherValidation(data);
+    return UserModel.query({
+        where: {email: data.email},
+        orWhere: { username: data.username }
+    }).fetch().then(
+        user=> {
+           if(user) {
+                if(user.get('username') === data.username) {
+                    errors.username = "Đã có người dùng khác đăng ký tên đăng nhập này, bạn hãy chọn tên đăng nhập khác"
+                }
+                if(user.get('email') === data.email) {
+                    errors.email = "Đã có người dùng khác sử dụng email này, bạn hãy chọn email khác"
+                }
+           }
+           
+           return {
+               errors,
+               isValid: isEmpty(errors)
+           }
+        }
+    )
+}
+
 
 UserRouters.post('/signup',  (req, res, next) => {
     const { username, email, password, passwordConfirm, gender, role, phone } = req.body;
     const user = { username, email, password, gender, phone, role }
-    const {errors, isValid} = validateInput(req.body);
-    if(isValid) {
-        const password_hash = bcrypt.hashSync(password, 10);
-        user.password = password_hash;
-        UserModel.forge(user, { hasTimestamps: true})
-            .save()
-            .then(
-                data => {
-                    console.log("data = ", data);
-                    res.json({ success: true, user: data})
-                }
-            )
-            .catch(                
-                err => {
-                    console.log(err);
-                    res.status(500).json({ error: err })
-                }
-            )
-    }
-    else {
-        res.status(400).json(errors);
-    }
-    // res.json({ username, email, password, passwordConfirm, gender, role, phone });
+
+    validateInput(req.body, commonValidation).then(
+        ({errors, isValid})=> {            
+            if(isValid) {
+                const password_hash = bcrypt.hashSync(password, 10);
+                user.password = password_hash;
+                UserModel.forge(user, { hasTimestamps: true})
+                    .save()
+                    .then(
+                        data => {
+                            console.log("data = ", data);
+                            res.json({ success: true, user: data})
+                        }
+                    )
+                    .catch(                
+                        err => {
+                            console.log(err);
+                            res.status(500).json({ error: err })
+                        }
+                    )
+            }
+            else {
+                res.status(400).json(errors);
+            }
+        }
+    );
+     
 });
 
 UserRouters.post('/login', (req, res, next)=> {
